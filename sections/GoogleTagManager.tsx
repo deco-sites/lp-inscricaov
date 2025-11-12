fbdfe91fb6ee378f69850f09a82ea56f296983a9
Rodrigo Soares <roodsoares@gmail.com>
2025-11-12 20:08:34 +0000

    New release by Rodrigo Soares

--- sections/GoogleTagManager.tsx
+++ sections/GoogleTagManager.tsx
@@ -1,39 +1,52 @@
-import { Head } from "$fresh/runtime.ts";
+import { AppContext } from "../apps/site.ts";
+import { SectionProps } from "@deco/deco";
 
 export interface Props {
   /**
-   * @title GTM Container ID
-   * @description Your Google Tag Manager Container ID (e.g., GTM-XXXXXXX)
+   * @title Google Tag Manager ID
+   * @description Your GTM container ID (GTM-XXXXXXX)
    */
-  containerId?: string;
+  trackingId: string;
 }
 
-export default function GoogleTagManager({ containerId = "GTM-XXXXXXX" }: Props) {
-  if (!containerId || containerId === "GTM-XXXXXXX") {
+export default function GoogleTagManager(
+  { trackingId }: SectionProps<typeof loader>,
+) {
+  if (!trackingId) {
     return null;
   }
 
   return (
     <>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${containerId}');`,
-          }}
-        />
-      </Head>
+      {/* Google Tag Manager - Head */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${trackingId}');
+          `,
+        }}
+      />
+
+      {/* Google Tag Manager - Body (noscript) */}
       <noscript>
         <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${containerId}`}
+          src={`https://www.googletagmanager.com/ns.html?id=${trackingId}`}
           height="0"
           width="0"
-          style="display:none;visibility:hidden"
-        />
+          style={{ display: "none", visibility: "hidden" }}
+        >
+        </iframe>
       </noscript>
     </>
   );
 }
+
+export const loader = (props: Props, _req: Request, ctx: AppContext) => {
+  return {
+    ...props,
+  };
+};
